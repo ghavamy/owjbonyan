d4df845b26d6d97ea55d1ffe924c8e17927d644f
ghavamy <ghavamy1374@gmail.com>
2025-04-14 18:27:42 +0330

    first stage of video posts created

--- oojbonyan/settings.py
+++ oojbonyan/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'embed_video',
+    'videos',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +134,9 @@ JAZZMIN_SETTINGS = {
     "copyright": "Owjbonyan Institute",
     "show_ui_builder": True,
 }
+
+#for medias
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
---
+++ videos/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns = [
+    path('', views.videoIndex, name='videoIndex'),
+    path('create/', views.CreateVideo.as_view(), name='create_video'),
+    path('<int:pk>/', views.DetailVideo.as_view(), name='video_detail'),
+    path('<int:pk>/update', views.UpdateVideo.as_view(), name='update_video'),
+    path('<int:pk>/delete', views.DeleteVideo.as_view(), name='delete_video'),
+
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
